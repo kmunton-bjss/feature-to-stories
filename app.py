eda1902ab005851bb51227b575aefb8878d14fc6
Kanta Munton <kanta.munton@bjss.com>
2024-03-19 16:27:18 +0000

    Add loading spinners and improve test prompt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 load_dotenv()
@@ -26,6 +26,9 @@ def home():
 def stories():
   id = request.args.get("id")
   res = queries.get(id)
+  if not res:
+    return redirect("/")
+  
   html = res.get("html")
   feature = res.get("feature")
   title = res.get("title")
@@ -47,17 +50,19 @@ def stories_result():
   
   completionStories = client.chat.completions.create(
     model=openai_deployment, # model = "deployment_name".
-    temperature=0.1,
+    temperature=0.5,
     messages=[
       {
           "role": "system", "content": "Act as a business analyst"
       },
       {
-          "role": "user", "content": f"""Create a list of JIRA stories, also known as user stories, 
-          based on an application's features. A feature should be broken down into two or more stories.
-          Describe the acceptance criteria for each story in the given, when and then format.
+          "role": "user", "content": f"""Breakdown a feature description into individual user stories. 
+          A feature should be broken down into two or more user stories. 
+          Each user story should have an informative title. 
+          Each user story should have a description that follows the format "As a [persona], I [want to], [so that]". 
+          Each user story should have several acceptance criteria in the given, when and then format. 
           
-          Return the answer in this HTML format for each story: {HTML_STORIES_FORMAT} 
+          Return the answer in this HTML format for each user story: {HTML_STORIES_FORMAT} 
 
           The feature is: {feature}"""
       },
@@ -70,7 +75,8 @@ def stories_result():
     temperature=0.5,
     messages=[
       {
-          "role": "user", "content": f"""Create a title for a feature description and only return the title. 
+          "role": "user", "content": f"""Create an informative title 
+          for a feature description and only return the title. 
           Feature description: {feature}"""
       },
     ]
@@ -105,13 +111,14 @@ def test_code():
       },
       {
           "role": "user", "content": f"""Based on user stories list comprehensive test scenarios 
-          with positive, negative and edge cases based on the acceptance criteria for each story. 
-          The tests should cover all types: integration, end-to-end, functional and non-functional tests.
-          Make sure there is at least the same number of tests (or more) as the number of acceptance criteria for each story. 
+          with positive, negative and edge case tests. 
+          Base the tests on the acceptance criteria for each story. 
+          When appropriate, also include non-functional tests. 
+          Make sure there is at least the same number of tests as the number of acceptance criteria for each story. 
           Include sample test data for each test and create sample code for each test scenario. 
-          Use Playwright tool syntax, Jest library syntax and JavaScript ES modules syntax for the sample code.
+          Use Playwright tool syntax, Jest library syntax and JavaScript ES modules syntax for the sample code. 
           
-          Return the answer in this HTML format for each test scenario: {HTML_TEST_FORMAT} 
+          Return the answer in this HTML format for all test scenarios: {HTML_TEST_FORMAT} 
 
           The stories are: {stories}"""
       },
@@ -154,7 +161,7 @@ HTML_TEST_FORMAT = """
   </h3>
   <div id="story{{ story title }}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#stories">
     <div class="accordion-body">
-      <p class="lead">{{ Test Type e.g. positive, negative, edge case, functional, non-functional, integration, end-to-end etc }}</p>
+      <p class="lead">Positive: {{ test title }}</p>
       <p><strong>Given</strong>: {{ given }}</p>
       <p><strong>When</strong>: {{ when }}</p>
       <p><strong>Then</strong>: {{ then }}</p>
@@ -163,7 +170,70 @@ HTML_TEST_FORMAT = """
         <div class="accordion-item">
           <h2 class="accordion-header" id="heading{{ given when then }}">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#code{{ given when then }}" aria-expanded="true" aria-controls="collapseOne">
-              See sample code in {{ coding language and tools }}
+              See sample test code
+            </button>
+          </h2>
+          <div id="code{{ given when then }}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion{{ given when then }}">
+            <div class="accordion-body">
+              <pre style="white-space: pre-wrap;">
+                <code>{{ test sample code }}</code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="lead">Negative: {{ test title }}</p>
+      <p><strong>Given</strong>: {{ given }}</p>
+      <p><strong>When</strong>: {{ when }}</p>
+      <p><strong>Then</strong>: {{ then }}</p>
+      <p><strong>test data</strong>: {{ test data }}</p>
+      <div class="accordion" id="accordion{{ given when then }}>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="heading{{ given when then }}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#code{{ given when then }}" aria-expanded="true" aria-controls="collapseOne">
+              See sample test code
+            </button>
+          </h2>
+          <div id="code{{ given when then }}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion{{ given when then }}">
+            <div class="accordion-body">
+              <pre style="white-space: pre-wrap;">
+                <code>{{ test sample code }}</code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="lead">Edge case: {{ test title }}</p>
+      <p><strong>Given</strong>: {{ given }}</p>
+      <p><strong>When</strong>: {{ when }}</p>
+      <p><strong>Then</strong>: {{ then }}</p>
+      <p><strong>test data</strong>: {{ test data }}</p>
+      <div class="accordion" id="accordion{{ given when then }}>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="heading{{ given when then }}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#code{{ given when then }}" aria-expanded="true" aria-controls="collapseOne">
+              See sample test code
+            </button>
+          </h2>
+          <div id="code{{ given when then }}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion{{ given when then }}">
+            <div class="accordion-body">
+              <pre style="white-space: pre-wrap;">
+                <code>{{ test sample code }}</code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="lead">Non-functional: {{ test title }}</p>
+      <p><strong>Given</strong>: {{ given }}</p>
+      <p><strong>When</strong>: {{ when }}</p>
+      <p><strong>Then</strong>: {{ then }}</p>
+      <p><strong>test data</strong>: {{ test data }}</p>
+      <div class="accordion" id="accordion{{ given when then }}>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="heading{{ given when then }}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#code{{ given when then }}" aria-expanded="true" aria-controls="collapseOne">
+              See sample test code
             </button>
           </h2>
           <div id="code{{ given when then }}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion{{ given when then }}">
