30665faeb04c7197cae534c7c4042439df9ae4e9
Kanta Munton <kanta.munton@bjss.com>
2024-03-19 11:38:39 +0000

    Update prompts

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def stories():
   res = queries.get(id)
   html = res.get("html")
   feature = res.get("feature")
-  return render_template("stories.html", html=html, feature=feature, id=id)
+  title = res.get("title")
+  return render_template("stories.html", html=html, feature=feature, id=id, title=title)
 
 
 @app.post("/stories")
@@ -41,19 +42,17 @@ def stories_result():
   if res != -1:
     return render_template("stories.html", html=res.get("html"), feature=feature, id=id)
   
-  completion = client.chat.completions.create(
+  completionStories = client.chat.completions.create(
     model=openai_deployment, # model = "deployment_name".
-    temperature=0.5,
+    temperature=0.1,
     messages=[
       {
-          "role": "system", "content": "Act as a business analyst and a quality assurance engineer."
+          "role": "system", "content": "Act as a business analyst"
       },
       {
-          "role": "user", "content": f"""Create a list of stories based on a feature. 
-          Describe the acceptance criteria for each story in the given, when and then format. 
-          List comprehensive test scenarios with positive, negative and edge cases based on the acceptance criteria for each story. 
-          Make sure there is at least the same number of tests as the number of acceptance criteria for each story. 
-          Include sample test data for each test. 
+          "role": "user", "content": f"""Create a list of JIRA stories, also known as user stories, 
+          based on an application's features. A feature should be broken down into more than one story.
+          Describe the acceptance criteria for each story in the given, when and then format.
           
           Return the answer in this HTML format for each story: {HTML_STORIES_FORMAT} 
 
@@ -61,35 +60,52 @@ def stories_result():
       },
     ]
   )
-  html = completion.choices[0].message.content
+  html = completionStories.choices[0].message.content
+  
+  completionTitle = client.chat.completions.create(
+    model=openai_deployment, # model = "deployment_name".
+    temperature=0.1,
+    messages=[
+      {
+          "role": "user", "content": f"""Create a title for a feature description and only return the title. 
+          Feature description: {feature}"""
+      },
+    ]
+  )
+  title = completionTitle.choices[0].message.content
+  title = title.replace('"', "")
 
   # Store in memory cache
-  queries[id] = {"html": html, "feature": feature, "test": ""}
+  queries[id] = {"html": html, "feature": feature, "title": title, "test": ""}
 
-  return render_template("stories.html", html=html, feature=feature, id=id)
+  return render_template("stories.html", html=html, feature=feature, id=id, title=title)
 
-@app.post("/stories/test-code")
+@app.post("/stories/tests")
 def test_code():
   id = request.form.get("id")
   res = queries.get(id)
+  title = res.get("title")
   
   # Get cached response
   test = res.get("test")
   if test:
-    return render_template("test_code.html", html=test, id=id)
+    return render_template("tests.html", html=test, id=id, title=title)
   
   stories = res.get("html")
   completion = client.chat.completions.create(
     model=openai_deployment, # model = "deployment_name".
-    temperature=0.5,
+    temperature=0.1,
     messages=[
       {
           "role": "system", "content": "Act as a quality assurance developer"
       },
       {
-          "role": "user", "content": f"""Based on stories and test scenarios, 
-          create sample code for each test scenario. 
-          Use Playwright, Jest and JavaScript for the code. 
+          "role": "user", "content": f"""Based on stories list comprehensive test scenarios 
+          with positive, negative and edge cases based on the acceptance criteria for each story. 
+          The tests should cover all types: integration, end-to-end, functional and non-functional tests.
+          Make sure there is at least the same number of tests (or more) as the number of acceptance criteria for each story. 
+          Include sample test data for each test and create sample code for each test scenario. 
+          Use Playwright tool syntax, Jest library syntax and JavaScript ES modules syntax for the sample code.
           
           Return the answer in this HTML format for each test scenario: {HTML_TEST_FORMAT} 
 
@@ -101,8 +117,8 @@ def test_code():
   
   # Store in memory cache
   queries[id]["test"] = html
-
-  return render_template("test_code.html", html=html, id=id)
+  
+  return render_template("tests.html", html=html, id=id, title=title)
 
 HTML_STORIES_FORMAT = """
 <h3>Story 1: {{story title}}</h3>
@@ -113,35 +129,46 @@ HTML_STORIES_FORMAT = """
   <li><strong>given</strong> {{ given }},<strong>when</strong> {{ when }}, <strong>then</strong> {{ then }}</li>
   <li><strong>given</strong> {{ given }},<strong>when</strong> {{ when }}, <strong>then</strong> {{ then }}</li>
 </ol>
-<h4>End to end test scenarios</h4>
+"""
+
+HTML_TEST_FORMAT = """
+<h2>{{ Story }}</h2>
+<p>{{ Story description }}</p>
+<h4>Test scenarios</h4>
 <h5>Positive</h5>
 <ul>
   <li>
-    <p><strong>scenario</strong>: {{ scenario }}</p>
-    <p><strong>expected result</strong>: {{ expected result }}</p>
+    <p><strong>Given</strong>: {{ given }}</p>
+    <p><strong>When</strong>: {{ when }}</p>
+    <p><strong>Then</strong>: {{ then }}</p>
     <p><strong>test data</strong>: {{ test data }}</p>
+    <pre style="border: 1px solid black; border-radius: 25px; padding: 10px; white-space: pre-wrap;">
+      <code>{{ test sample code }}</code>
+    </pre>
   </li>
 </ul>
 <h5>Negative</h5>
 <ul>
   <li>
-    <p><strong>scenario</strong>: {{ scenario }}</p>
-    <p><strong>expected result</strong>: {{ expected result }}</p>
+    <p><strong>Given</strong>: {{ given }}</p>
+    <p><strong>When</strong>: {{ when }}</p>
+    <p><strong>Then</strong>: {{ then }}</p>
     <p><strong>test data</strong>: {{ test data }}</p>
+    <pre style="border: 1px solid black; border-radius: 25px; padding: 10px; white-space: pre-wrap;">
+      <code>{{ test sample code }}</code>
+    </pre>
   </li>
 </ul>
 <h5>Edge cases</h5>
 <ul>
   <li>
-    <p><strong>scenario</strong>: {{ scenario }}</p>
-    <p><strong>expected result</strong>: {{ expected result }}</p>
+    <p><strong>Given</strong>: {{ given }}</p>
+    <p><strong>When</strong>: {{ when }}</p>
+    <p><strong>Then</strong>: {{ then }}</p>
     <p><strong>test data</strong>: {{ test data }}</p>
+    <pre style="border: 1px solid black; border-radius: 25px; padding: 10px; white-space: pre-wrap;">
+      <code>{{ test sample code }}</code>
+    </pre>
   </li>
 </ul>
-"""
-
-HTML_TEST_FORMAT = """
-<h2>{{ Test scenario }}</h2>
-<p>{{ Test description }}</p>
-<pre style="border: 1px solid black; border-radius: 25px; padding: 10px"><code>{{ test code }}</code></pre>
 """
